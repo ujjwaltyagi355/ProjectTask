0f345918ef40dcaf2676515f05817d07cd595c7e
ujjwaltyagi355 <ujjwaltyagi355@gmail.com>
2021-04-03 15:54:17 +0530

    Initial Commit

---
+++ React Files/src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render } from 'react-dom';
+
+import { history } from './_helpers';
+import { accountService } from './_services';
+import { App } from './app';
+
+import './styles.less';
+
+
+// attempt silent token refresh before startup
+accountService.refreshToken().finally(startApp);
+
+function startApp() { 
+    render(
+        <Router history={history}>
+            <App />
+        </Router>,
+        document.getElementById('app')
+    );
+}
